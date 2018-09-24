d8bfd5f2ded72a15703b5b97958c819ea077941e
imranmomin <imran.amomin@outlook.com>
2018-09-24 15:51:39 -0400

    improve to handle error while setting job state

--- Hangfire.AzureDocumentDB/StoredProcedure/setJobState.js
+++ Hangfire.AzureDocumentDB/StoredProcedure/setJobState.js
@@ -6,40 +6,39 @@
  * @param {Object} state - the state information for the job
  */
 function setJobState(id, state) {
-    var result = __.filter(function (doc) {
-        return doc.id === id && doc.type === 2;
-    }, function (err, docs) {
-        if (err) throw err;
-        if (docs.length === 0) throw new Error("No job found for id :" + id);
-        if (docs.length > 1) throw new Error("Found more than one job for id :" + id);
+    let response = getContext().getResponse();
+    let filter = (doc) => doc.type === 2 && doc.id === id;
 
-        // create the state document
-        createState(state, function (document) {
-            var doc = docs[0];
-            doc.state_id = document.id;
+    let result = __.filter(filter, (err, docs) => {
+        if (err) throw err;
+        if (docs.length === 0) throw new Error(`No job found for id :${id}`);
+        if (docs.length > 1) throw new Error(`Found more than one job for id :${id}`);
+
+        let createResult = __.createDocument(__.getSelfLink(), state, (err, resp) => {
+            if (err) throw err;
+
+            let doc = docs.shift();
+            doc.state_id = resp.id;
             doc.state_name = state.name;
 
-            var isAccepted = __.replaceDocument(doc._self, doc, function (error) {
+            let replaceResult = __.replaceDocument(doc._self, doc, (error) => {
                 if (error) throw error;
             });
 
-            if (!isAccepted) throw new Error("Failed to set the state to job");
-            else getContext().getResponse().setBody(true);
-
+            if (replaceResult) {
+                throw new Error("Failed to set the state to job");
+            } else {
+                response.setBody(true);
+            }
         });
+
+        if (createResult) {
+            throw new Error("Unable to create the state document");
+        }
+
     });
 
-    if (!result.isAccepted) throw new Error("The call was not accepted");
-
-    /**
-     * Creates a new state
-     * @param {Object} state - information for the job 
-     * @param {function} callback - return the newly create state document
-     */
-    function createState(state, callback) {
-        __.createDocument(__.getSelfLink(), state, function (err, document) {
-            if (err) throw err;
-            callback(document);
-        });
+    if (result) {
+        throw new Error("The call was not accepted");
     }
 }
