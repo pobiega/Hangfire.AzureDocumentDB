84454a0ffe2f9cbcc200be7f25bd831316a2b6a5
imranmomin <imran.amomin@outlook.com>
2018-09-24 15:35:32 -0400

    improved the sp setJobParameter

--- Hangfire.AzureDocumentDB/StoredProcedure/setJobParameter.js
+++ Hangfire.AzureDocumentDB/StoredProcedure/setJobParameter.js
@@ -7,39 +7,36 @@
  * @param {string} value - the value of the parameter
  */
 function setJobParameter(id, name, value) {
-    var result = __.filter(function (doc) {
-        return doc.type === 2 && doc.id === id;
-    }, function (err, docs) {
+    let response = getContext().getResponse();
+    let filter = (doc) => doc.type === 2 && doc.id === id;
+
+    let result = __.filter(filter, (err, docs) => {
         if (err) throw err;
         if (docs.length === 0) throw new Error("No job found for id :" + id);
         if (docs.length > 1) throw new Error("Found more than one job for id :" + id);
 
-        var doc = docs[0];
-        if (!doc.parameters) {
+        let doc = docs.shift();
+
+        // when the parameter is empty/null/undefiend
+        if (doc.parameters === undefined || doc.parameters === null || doc.parameters.length === 0) {
             doc.parameters = [{ name: name, value: value }];
         } else {
-            var parameter = null;
-            for (var index = 0; index < doc.parameters - 1; index++) {
-                parameter = doc.parameters[index];
-                if (parameter.name === name) {
-                    parameter.value = value;
-                    break;
-                }
-                parameter = null;
-            }
-
-            if (!parameter) {
+            // try to find the parameter
+            let parameter = docs.parameters.find(p => p.name === name);
+            if (parameter) {
+                parameter.value = value;
+            } else {
                 doc.parameters.push({ name: name, value: value });
             }
         }
 
-        var isAccepted = __.replaceDocument(doc._self, doc, function(error) {
+        let replaceResult = __.replaceDocument(doc._self, doc, function (error) {
             if (error) throw error;
         });
 
-        if (!isAccepted) throw new Error("Failed to set the parameter");
-        else getContext().getResponse().setBody(true);
+        if (!replaceResult) throw new Error("Failed to set the parameter");
+        else response.setBody(true);
     });
 
-    if (!result.isAccepted) throw new Error("The call was not accepted");
+    if (!result) throw new Error("The call was not accepted");
 }
